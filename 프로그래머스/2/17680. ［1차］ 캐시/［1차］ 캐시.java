146bb546aec6f2e77c1d2f8914bee54d62e1b6c8
Tcimel <61877802+Tcimel@users.noreply.github.com>
2025-06-01 15:07:27 +0900

    [level 2] Title: [1차] 캐시, Time: 31.93 ms, Memory: 126 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
@@ -1,31 +1,24 @@
-import java.util.LinkedHashMap;
+import java.util.LinkedList;
 
 class Solution {
     public int solution(int cacheSize, String[] cities) {
-        if(cacheSize == 0 ) return cities.length * 5;
+        if(cacheSize == 0) return cities.length * 5;
 
 		int time = 0;
-		LinkedHashMap<String, Integer> cache = new LinkedHashMap<>(cacheSize,0.75f, true);
-		// cacheSize : 캐시 사이즈 설정
-		// 0.75f : 기본 값, 얼마나 채워졌을 때 리사이징 할 지 결정
-		// true : get 하면 맨 뒤로 이동 됨
-
-		for(String city : cities){
+		LinkedList<String> cache = new LinkedList<>();
+		for (String city : cities){
 			city = city.toLowerCase();
 
-			if(cache.containsKey(city)){
-				//캐시 안에 존재 할 경우, 속도 1로 계산
+			if(cache.contains(city)){
 				time += 1;
-				cache.get(city);
-			}else {
-				//캐시 MISS
+				cache.remove(city);
+			}else{
 				time += 5;
 				if(cache.size() >= cacheSize){
-					String oldest = cache.keySet().iterator().next();
-					cache.remove(oldest);
+					cache.poll();
 				}
-				cache.put(city,0);
 			}
+			cache.offer(city);
 		}
 		return time;
     }
